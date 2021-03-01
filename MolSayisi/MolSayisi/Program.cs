9e4ef8d3bea632445112bb3953550e238f59c1c0
Sefa Kozan <sefakozan05@gmail.com>
2021-03-01 22:40:13 +0300

    Mask Model v2 tekrar kullanıldı.

---
+++ MolSayisi/MolSayisi/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MolSayisi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        static void AtomSayisiAl()
+        {
+            Console.WriteLine("CxHyOz bileşiğinin atom sayılarını giriniz.");
+            Console.Write("x: ");
+            int xAtomSayisi = Convert.ToInt32(Console.ReadLine());
+            Console.Write("y: ");
+            int yAtomSayisi = Convert.ToInt32(Console.ReadLine());
+            Console.Write("z: ");
+            int zAtomSayisi = Convert.ToInt32(Console.ReadLine());
+        }
+        static void MolekulSayisiHesapla()
+        {
+
+        }
+        static void Hacmi()
+        {
+
+        }
+    }
+}
